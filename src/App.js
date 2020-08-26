5f0526497e9b0754363403b5ff8933d1f715c226
britniangela <66388266+britniangela@users.noreply.github.com>
2020-08-26 12:28:57 -0400

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/britniangela/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open-Source Code
+        </a>{" "}
+        by Britni Burdette
+      </footer>
+    </div>
+  );
 }
