3339354c6e41a42cdfd1a969f7994ee1f98a83ea
britniangela <66388266+britniangela@users.noreply.github.com>
2020-08-30 08:41:55 -0400

    Cleaned up code

--- src/Weather.js
+++ src/Weather.js
@@ -72,7 +72,6 @@ export default function Weather(props) {
     );
   } else {
     const apiKey = "137e68217e6f5991d5d7befae9ff8e52";
-
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&unit=fahrenheit`;
     axios.get(apiUrl).then(handleResponse);
 
